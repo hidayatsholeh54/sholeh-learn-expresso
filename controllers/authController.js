f74a3d57edc194a992a67b28f79a85af396a1cc6
hidayatsholeh54 <hidayatsholeh54@gmail.com>
2026-04-15 11:24:20 +0700

    tambah update, delete dan change password

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,8 +49,133 @@ exports.login = async (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: "1d"}
         );
-        res.json({ message: "Login Berhasil!", token });
+        res.json({ message: "Login Berhasil!", token, user: { id: user.id, email:user.email, role: user.role }});
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
+};
+
+exports.createAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validasi basic
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email & password wajib" });
+    }
+
+    // cek user sudah ada atau belum
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User sudah ada" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create admin
+    const admin = await User.create({
+      email,
+      password: hashedPassword,
+      role: "admin",
+    });
+
+    res.json({
+      message: "Admin berhasil dibuat",
+      data: {
+        id: admin.id,
+        email: admin.email,
+        role: admin.role,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// update user
+exports.updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email, role } = req.body;
+
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User tidak ditemukan" });
+    }
+
+    // update field (hanya yang dikirim)
+    if (email) user.email = email;
+    if (role) user.role = role;
+
+    await user.save();
+
+    res.json({
+      message: "User berhasil diupdate",
+      user: {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// change password
+exports.changePassword = async (req, res) => {
+    try {
+        const { oldPassword, newPassword } = req.body;
+
+        // validasi input
+        if (!oldPassword || !newPassword) {
+            return res.status(400).json({ message:"Old password dan new password wajib diisi" })
+        }
+
+        // ambil user dari middleware (req.user)
+        const user = await User.findByPk(id);
+
+        if(!user) {
+            return res.status(400).json({ message: "user tidak ditemukan" });
+        }
+
+        // check password lama 
+        const isMatch = await bcrypt.compare(oldPassword, user.password);
+        console.log(isMatch, "match password");
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Password lama salah" });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
+// delete user 
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User tidak ditemukan" });
+    }
+
+    if (req.user.id == id) {
+      return res.status(400).json({ message: "Tidak bisa hapus diri sendiri" });
+    }
+
+    // destroy adalah method milik sequelize untuk menghapus baris data
+    await user.destroy();
+
+    res.json({
+      message: "User berhasil dihapus",
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { register, login} = require("../controllers/authController");
+const { register, login, createAdmin, updateUser, deleteUser, changePassword} = require("../controllers/authController");
 const { verifyToken, checkRole } = require("../middlewares/authMiddleware");
+const User = require("../models/user");
 
 // public route
 router.post("/register", register);
 router.post("/login", login);
 
 // admin
+router.post("/create-admin", verifyToken, checkRole("admin"), createAdmin );
+router.put("/users/:id", verifyToken, checkRole("admin"), updateUser);
+router.delete("/users/:id", verifyToken, checkRole("admin"), deleteUser);
+router.put("/change-password", verifyToken, changePassword);
+
 router.get("/admin", verifyToken, checkRole("admin"), (req, res) => {
   res.json({ message: "Welcome admin" });
 });
@@ -15,6 +21,27 @@ router.get("/admin", verifyToken, checkRole("admin"), (req, res) => {
 // protected route
 router.get("/profile", verifyToken, (req, res) => {
     res.json({ message: "Berhasil Akses Profile", user: req.user });
+});
+
+// ambil data user berdasarkan token masuk
+router.get("/me", verifyToken, (req, res) => {
+  res.json({
+    message: "Berhasil ambil data user",
+    user: {
+      id: req.user.id,
+      email: req.user.email,
+      role: req.user.role
+    }
+  })
+})
+
+// 
+router.get("/users", verifyToken, checkRole("admin"), async (req, res) => {
+  const users = await User.findAll({
+    attributes: ["id", "email", "role"],
+  });
+
+  res.json(users);
 });
 
 module.exports = router;
